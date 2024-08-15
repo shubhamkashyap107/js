fde61c8cf971baa546ce3bc03032eac7d04e0c32
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-08-15 15:07:00 +0530

    Commit

---
+++ day3/Async Programming/index.js
@@ -0,0 +1,47 @@
+// console.log("Pakode banao");
+
+
+
+// for(let i = 0; i < 999999999; i++)
+// {
+
+// }
+// console.log("Sauce is here");
+
+
+// console.log("Pakode kha lo");
+
+
+
+
+
+// console.log("Pakode banao");
+
+// setTimeout(() => {
+// console.log("Sauce is here");
+// }, 5000)
+
+// console.log("Pakode kha lo");
+
+
+let count = 0;
+
+const intervalID = setInterval(() => {
+    console.log("Set timer chal gya")
+    // count = count + 1
+    // if(count == 4)
+    // {
+    //     console.log("Interval cleared")
+    //     clearInterval(intervalID)
+    // }
+}, 2000)
+
+
+setTimeout(() => {
+    clearInterval(intervalID)
+}, 10000)
+
+
+
+
+
